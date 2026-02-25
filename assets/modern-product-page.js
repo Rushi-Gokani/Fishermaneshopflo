2c6c1a002672c15a3fbdf4be47c39cb035f7017e
Rushi Gokani <rushigokani124@gmail.com>
2026-02-25 12:49:57 +0530

    feat: Add a new slideshow component and a card gallery snippet.

--- assets/modern-product-page.js
+++ assets/modern-product-page.js
@@ -618,7 +618,7 @@ class ModernProductPage {
 
         // Publish event
         this.publishCartEvent();
-        handleFloCartBtn();
+        if (typeof handleFloCartBtn === 'function') handleFloCartBtn();
 
         setTimeout(() => {
           if (defaultText) defaultText.style.display = '';
